70f77e3beff0799dc5e2ac2867339f9fbf539b8a
Orisa123321 <136339149+Orisa123321@users.noreply.github.com>
2026-04-12 12:31:32 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'הרשימה של הבית',
+        short_name: 'קניות', /* השם הקצר שיופיע מתחת לאייקון בטלפון */
+        description: 'אפליקציה חכמה לניהול קניות הבית',
+        theme_color: '#4CAF50', /* הצבע הירוק של הכפתורים שלנו */
+        background_color: '#ffffff',
+        display: 'standalone', /* זה מה שמעלים את שורת הדפדפן! */
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
